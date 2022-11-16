07b0d586534b4c882ea933ebb5b99a5c6e165079
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2022-11-16 15:35:05 -0800

    Added replit notes

---
+++ class-06/in-class-demo/script.js
@@ -0,0 +1,69 @@
+// NOTES FROM THE REPLIT
+
+// let a = 1;
+// let b = 2;
+// let c = a + b;
+
+// // variables - containers for storing data
+// console.log(a);
+// console.log(c);
+// console.log("Hello world!");
+// console.log(42);
+// console.log(42 + 9);
+
+// variables can be declared 4 ways
+// const --> constant, can't be changed
+// let --> changeable value
+// var --> old way, don't do this
+// (none) --> don't worry about this for now
+
+// const myName = "Kassie";
+// console.log(myName);
+// // myName = "Teach"; <-- this will break
+
+// let myFavColor = "yellow";
+// console.log(myFavColor);
+// myFavColor = "blue";
+// console.log(myFavColor);
+
+// DATA TYPES
+// strings --> 'text' or "text" or '2' or "2"
+// numbers/integers --> 2 or 42 or 4738295747395
+// boolean --> true or false
+
+// USEFUL FUNCTIONS
+// console.log() --> print information to the console
+// alert() --> give us a popup box
+// prompt() --> how you can get info from the user
+// document.write() --> how you will put content on your website
+
+// CONDITIONAL LOGIC (if/else if/else)
+let usersName = prompt("What is your name?");
+console.log(usersName);
+// usersName = usersName.toLowerCase();
+
+
+if(usersName.toLowerCase() == "kassie"){
+  alert("Hiya teach!");
+  console.log("I am in the if-statement")
+} else if (usersName == "ben") {
+    alert("TA's are awesome");
+  console.log("I am in the else-if-statement");
+} else if (usersName == "aaron") {
+  alert("TA's are awesome");
+} else if (usersName == "tammy") {
+  alert("TA's are awesome");
+} else if (usersName == "joel") {
+  alert("TA's are awesome");
+} else {
+  alert("Hello and welcome, class");
+  console.log("I am in the else-statement")
+}
+
+// // OPERATORS
+
+// // let myNumber = 42
+// // console.log(myNumber == '42');
+
+
+
